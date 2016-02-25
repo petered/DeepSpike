bddcfc3688f005a4425fa27f98dac681710f0339
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-02-25 12:21:09 +0100

    added some stuff to make it install all nice

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+
+setup(name='DeepStream',
+      author='Peter',
+      author_email='poconn4@gmail.com',
+      url='https://github.com/petered/deepstream',
+      long_description='A Java library for Spiking Neural Networks',
+      version=0,
+      packages=['DeepStream'],
+      scripts=[])
