fc8f8df7d1f333a189f0dcc1c125cd7bfd4b47ca
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-02-25 13:57:29 +0100

    tried push

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 
 from setuptools import setup
 
+import imp
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+foo = imp.load_source('deepstream', THIS_DIR+'/src/main/python/deepstream/deepstream_python_directory.py')
+foo.MyClass()
+
 
 setup(name='DeepStream',
       author='Peter',
       author_email='poconn4@gmail.com',
       url='https://github.com/petered/deepstream',
-      long_description='A Java library for Spiking Neural Networks',
+      long_description='A Java library for Spiking Deep Neural Networks',
       version=0,
       packages=[],
       scripts=[])
