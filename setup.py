065ad941e3d968ba0365cb988002cc5bcc98c341
Peter O'Connor <peter.ed.oconnor@gmail.com>
2016-02-26 11:01:10 +0100

    end of the short-lived deep-stream

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 
 from setuptools import setup
 
-setup(name='DeepStream',
+setup(name='DeepSpike',
       author='Peter',
       author_email='poconn4@gmail.com',
-      url='https://github.com/petered/deepstream',
+      url='https://github.com/petered/deepspike',
       long_description='A Java library for Spiking Deep Neural Networks',
       version=0,
       packages=[],
